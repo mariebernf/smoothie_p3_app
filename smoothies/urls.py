8d426ad21488ed6ce116a4c1095a5ea7b5ab2a1c
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-15 06:32:30 +0100

    Fix CSS test page 404 and confirm static CSS loads

--- smoothies/urls.py
+++ smoothies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('css_test/', views.css_test, name='css_test'),
 ]
--- smoothies/views.py
+++ smoothies/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'smoothies/home.html')
+
+def css_test(request):
+    return render(request, 'smoothies/base.html')
