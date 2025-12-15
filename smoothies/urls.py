c1a373937d45467bd2638b8ddf146289320f9b34
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-15 07:09:37 +0100

    Set smoothie list as the home page and remove test views

--- smoothies/urls.py
+++ smoothies/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('css_test/', views.css_test, name='css_test'),
 ]
--- smoothies/views.py
+++ smoothies/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'smoothies/home.html')
-
-def css_test(request):
-    return render(request, 'smoothies/base.html')
+    return render(request, 'smoothies/smoothie_list.html')
