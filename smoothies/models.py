d95d6fd7cb785da870626e53a721fa75018512c3
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-15 07:36:56 +0100

    Add smoothie model

--- smoothies/models.py
+++ smoothies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Smoothie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="smoothies"
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
