a09160de37a3887f72e9bb34cabc3e0a27437ed9
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-15 07:56:04 +0100

    Add smoothie list view

--- smoothies/views.py
+++ smoothies/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Smoothie
 
-def home(request):
-    return render(request, 'smoothies/smoothie_list.html')
+def smoothie_list(request):
+    smoothies = Smoothie.objects.all().order_by('-date_created')
+    return render(
+        request,
+        'smoothies/smoothie_list.html',
+        {'smoothies': smoothies}
+    )
